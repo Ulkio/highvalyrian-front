bf1996ef49d7398c9887c09b5584fb09e2f3e2a6
Guillaume <guillaume.verschave@gmail.com>
2024-07-19 11:17:12 +0200

    change discord link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react'
 
 const Footer = () => {
-  return (
-    <footer className="bg-navbar-dark w-full fixed bottom-0 flex justify-center items-center ">
-      <p className="text-xs">
-        Inspired by the glyphs created by{" "}
-        <a className="underline" href="https://dedalvs.tumblr.com/">
-          David J. Peterson
-        </a>{" "}
-        for his work on the valyrian language used in Game of Thrones and House of the Dragon. Using resources made by
-        the great community at{" "}
-        <a className="underline" href="https://discord.gg/TGZpdfy3">
-          https://discord.gg/jqXPXDhYtp
-        </a>
-      </p>
-    </footer>
-  );
-};
+    return (
+        <footer className='bg-navbar-dark w-full fixed bottom-0 flex justify-center items-center '>
+            <p className='text-xs'>
+                Inspired by the glyphs created by{' '}
+                <a className='underline' href='https://dedalvs.tumblr.com/'>
+                    David J. Peterson
+                </a>{' '}
+                for his work on the valyrian language used in Game of Thrones and House of the
+                Dragon. Using resources made by the great community at{' '}
+                <a className='underline' href='https://discord.gg/TGZpdfy3'>
+                    https://discord.gg/WZ2FrEUhqb
+                </a>
+            </p>
+        </footer>
+    )
+}
 
-export default Footer;
+export default Footer
